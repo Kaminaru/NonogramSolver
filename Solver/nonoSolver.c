8dfd61bbef5b45bc84efcb300726b6a40f9559c7
Kaminaru <Kaminaru.Daniel@gmail.com>
2020-06-17 13:46:23 +0000

    Test3

---
+++ Solver/nonoSolver.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+unsigned char n
+
+
+
+
+
+
+
+
+
+int main(int argc, char * argv[]){
+  if(argc < 2){
+    printf("usage: ./nanoSolver fileName\n");
+    return EXIT_FAILURE;
+  }
+  // 2D array setup
+
+}
